6a9f891df686f5f4d0fa15f32f0eb685b68ffe67
Divyansh Nautiyal <ndivyansh29@gmail.com>
2023-04-05 15:39:18 +0530

    leds

--- barcode.py
+++ barcode.py
@@ -1,26 +1,47 @@
+import RPi.GPIO as GPIO
+import time
 import cv2
 from pyzbar.pyzbar import decode
 import requests
-#cap = cv2.VideoCapture(0)
-cap=cv2.VideoCapture("http://<IPaddress:portnumber>/video")
 
-while(True):
-    ret,frame=cap.read()
-    barcodes=decode(frame)
+# Setup GPIO pins for LEDs------------------------
+green_pin = 17
+red_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(green_pin, GPIO.OUT)
+GPIO.setup(red_pin, GPIO.OUT)
+#----------------------------------------------
+#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("http://172.16.6.232:4747/video")
+
+while True:
+    ret, frame = cap.read()
+    barcodes = decode(frame)
     for barcode in barcodes:
         x, y, w, h = barcode.rect
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        barcode_data=barcode.data.decode("utf-8");
-        print("data :",barcode_data);
-        response=requests.post('http://<ipaddress>:8000/getBarcode',{'productID':str(barcode_data)});
-        if(response.status_code==200):
-            print("Recieved at backend")
+        barcode_data = barcode.data.decode("utf-8")
+        print("data:", barcode_data)
+        response = requests.post('http://172.16.6.150:8000/getBarcode', {'productID': str(barcode_data)})
+        if response.status_code == 200:
+            # Blink green LED
+            GPIO.output(green_pin, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(green_pin, GPIO.LOW)
+            print("Received at backend")
         else:
-            print("Error : ",str(response.status_code))
+            # Blink red LED
+            GPIO.output(red_pin, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(red_pin, GPIO.LOW)
+            print("Error:", str(response.status_code))
         print("----------------")
-        
+
     cv2.imshow('Barcode Scanner', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+# Clean up GPIO pins
+GPIO.cleanup()
 cap.release()
 cv2.destroyAllWindows()
